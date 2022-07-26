bf9e1705e84d54d5adb30f7c3966849f96cd77e3
Kacper Rolewski <kacper.r17@gmail.com>
2022-07-26 20:10:49 +0200

    Added Main Panel

--- app/src/main/java/com/example/passit/AddSubject.java
+++ app/src/main/java/com/example/passit/AddSubject.java
@@ -2,19 +2,13 @@ package com.example.passit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button addSubjectButton;
+public class AddSubject extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_add_subject);
     }
 }
